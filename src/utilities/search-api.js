ec52b832e5d34943b81bafcd3b97a80d989ac880
kailahk <kailahkorsh@me.com>
2023-05-29 13:10:42 -0700

    updates search-api

--- src/utilities/search-api.js
+++ src/utilities/search-api.js
@@ -1,11 +1,11 @@
 export async function getRepos(searchInput) {
     const options = {
         headers: {
-            accept: "application/vnd.github+json",
+            accept: "application/vnd.github.text-match+json",
+            "Content-Type": "application/json",
         },
     }
-    const queryString = `q=${searchInput}+in:name,description`;
-    const results = fetch(`https://api.github.com/search/repositories?${queryString}`, options
+    const results = fetch(`https://api.github.com/search/repositories?q=${searchInput}+in:name,description`, options
     ).then(res => res.json());
     return results
 }
